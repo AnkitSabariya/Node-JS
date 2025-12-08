bb49ec459c0dd41c5f3716fa4fd675ae03c13433
Mr Ankit <ankitsabariya@gmail.com>
2025-12-08 09:42:39 +0530

    TodoList

--- 5 TodoList/backend/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const database = () =>{
-mongoose.connect("mongodb://localhost:27017/todolist",{})
-.then(()=> console.log("DB connected"))
-.catch((err)=> console.error("DB error",err))
-}
-module.exports = database;
--- 5 TodoList/backend/models/model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const todoSchema = new mongoose.Schema({
-  todo: {
-    type: String,
-    required: [true, "Please Enter Task..."],
-  }
-});
-module.exports = mongoose.model("Todo",todoSchema)
