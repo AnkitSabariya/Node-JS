ea77888470e50357a8a73077c0b76921bd4682f6
Mr Ankit <ankitsabariya@gmail.com>
2025-12-17 17:12:03 +0530

    tg

--- Node-js-main/Mongo Db/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const app = express()
-
-app.listen(process.env.PORT, ()=>{
-    console.log('====================================');
-    console.log(`Server Is Running port: ${process.env.PORT} `);
-    console.log('====================================');
-})
